44df2082426bfe6953d256344342536c3148eda2
Harim Kim <rimiha9708@gmail.com>
2023-12-13 17:33:56 +0900

    404 page 작업

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,3 +1,48 @@
+import { Box, Flex } from "@chakra-ui/layout";
+import styled from "styled-components";
+import { IMG_URL } from "../constants";
+import { Link } from "react-router-dom";
+
+const MainBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const WrapSet = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const DuckImg = styled.div`
+  width: 300px;
+  height: 300px;
+  background: url(${IMG_URL}/1182224641134170152/1184412008641204276/404.png)
+    no-repeat center / contain;
+`;
+
+const BtnWrap = styled.button`
+  width: 80%;
+  border: none;
+  border-radius: 20px;
+  font-size: 30px;
+  font-weight: 900;
+  background-color: #fff;
+  text-align: center;
+  transition: 0.5s;
+
+  a {
+    color: #674211;
+    line-height: 50px;
+  }
+
+  &:hover {
+    background: #ffebd1;
+  }
+`;
+
 export const PageNotFound = () => {
   return (
     <>
@@ -9,7 +54,15 @@ export const PageNotFound = () => {
           bgColor="#c89f68"
           direction="column"
           {...WrapSet}
-        ></Flex>
+        >
+          <MainBox>
+            <DuckImg />
+
+            <BtnWrap>
+              <Link to={`/`}>메인화면으로</Link>
+            </BtnWrap>
+          </MainBox>
+        </Flex>
       </Box>
     </>
   );
