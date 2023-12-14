4d597e4bffe0244a761d63b7b99d0ae7a770b0bf
onyyyyy <hongki365@gmail.com>
2023-12-14 15:44:35 +0900

    유형테스트 작업2

--- src/pages/choice/Choice.js
+++ src/pages/choice/Choice.js
@@ -4,6 +4,7 @@ import { IMG_URL } from "../../constants";
 import { useState } from "react";
 import { Result } from "../result/Result";
 import { PageTitle } from "../../components/PageTitle";
+import { questions } from "../../components/questions";
 
 const MainBox = styled.div`
   display: flex;
@@ -76,97 +77,6 @@ const Answer = styled.button`
 
 export const Choice = () => {
   const [page, setPage] = useState(1);
-
-  const questions = [
-    {
-      q: ["질문1"],
-      a: [
-        { type: "I", text: "답변1" },
-        { type: "E", text: "답변2" },
-      ],
-    },
-    {
-      q: ["질문 2"],
-      a: [
-        { type: "I", text: "답변3" },
-        { type: "E", text: "답변4" },
-      ],
-    },
-    {
-      q: ["질문 3"],
-      a: [
-        { type: "I", text: "답변5" },
-        { type: "E", text: "답변6" },
-      ],
-    },
-    {
-      q: ["질문 4"],
-      a: [
-        { type: "S", text: "답변7" },
-        { type: "N", text: "답변8" },
-      ],
-    },
-    {
-      q: ["질문 5"],
-      a: [
-        { type: "S", text: "답변9" },
-        { type: "N", text: "답변10" },
-      ],
-    },
-    {
-      q: ["질문 6"],
-      a: [
-        { type: "S", text: "답변11" },
-        { type: "N", text: "답변12" },
-      ],
-    },
-    {
-      q: ["질문 7"],
-      a: [
-        { type: "F", text: "답변13" },
-        { type: "T", text: "답변14" },
-      ],
-    },
-    {
-      q: ["질문 8"],
-      a: [
-        { type: "F", text: "답변15" },
-        { type: "T", text: "답변16" },
-      ],
-    },
-    {
-      q: ["질문 9"],
-      a: [
-        { type: "F", text: "답변17" },
-        { type: "T", text: "답변18" },
-      ],
-    },
-    {
-      q: ["질문 10"],
-      a: [
-        { type: "P", text: "답변19" },
-        { type: "J", text: "답변20" },
-      ],
-    },
-    {
-      q: ["질문 11"],
-      a: [
-        { type: "P", text: "답변21" },
-        { type: "J", text: "답변22" },
-      ],
-    },
-    {
-      q: ["질문 12"],
-      a: [
-        { type: "P", text: "답변23" },
-        { type: "J", text: "답변24" },
-      ],
-    },
-    {
-      q: ["문제가 끝이 났어요! 결과 확인하러 갈까요?"],
-      a: [{ type: "", text: "결과보러가기" }],
-    },
-  ];
 
   const [mbtiValue, setMbtiValue] = useState([
     { name: "I", count: 0 },
