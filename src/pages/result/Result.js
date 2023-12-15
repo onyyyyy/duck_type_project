63c2799f9d6c1179237a25367e9f8b874aaae488
Harim Kim <rimiha9708@gmail.com>
2023-12-15 15:14:40 +0900

    result page ui

--- src/pages/result/Result.js
+++ src/pages/result/Result.js
@@ -1,5 +1,8 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { PageTitle } from "../../components/PageTitle";
+import { IMG_URL } from "../../constants";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const WrapSet = {
   display: "flex",
@@ -7,19 +10,70 @@ const WrapSet = {
   justifyContent: "center",
 };
 
+const DuckImg = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    max-width: 480px;
+  }
+`;
+
+const BottomWrap = styled.div`
+  width: 80%;
+`;
+
+const BottomBtn = styled.button`
+  all: unset;
+  width: 100%;
+  height: 80px;
+  font-size: 30px;
+  font-weight: 900;
+  background-color: #fff;
+  text-align: center;
+  transition: 0.5s;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px 0;
+  box-shadow: 10px 10px 5px #674211;
+
+  a {
+    color: #674211;
+    line-height: 50px;
+  }
+
+  &:hover {
+    background: #ffebd1;
+  }
+`;
+
 export const Result = () => {
   return (
     <>
       <PageTitle titleName="결과페이지" />
+      <DuckImg>
+        <img
+          src="https://cdn.discordapp.com/attachments/1182224641134170152/1184917586765434920/test_img.png"
+          alt="mbti오리"
+        />
+      </DuckImg>
       <Box {...WrapSet}>
         <Flex
           maxW="480px"
           w="100%"
-          h="100vh"
           bgColor="#c89f68"
           direction="column"
           {...WrapSet}
-        ></Flex>
+        >
+          <BottomWrap>
+            <BottomBtn>오리 모아보기</BottomBtn>
+            <BottomBtn>
+              <Link to="/">테스트 다시하기</Link>
+            </BottomBtn>
+          </BottomWrap>
+        </Flex>
       </Box>
     </>
   );
